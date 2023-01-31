a63e8f6f2889e0e050ff51c557ba0ee4332981c9
Rodrigo F. Coutinho <cvc.francisco@gmail.com>
2023-01-31 06:04:08 -0300

    populando linhas da tabela

--- frontend/src/components/ReportNew.js
+++ frontend/src/components/ReportNew.js
@@ -24,46 +24,50 @@ const columns = [
     type: 'number',
   },
   {
-    field: 'Co2',
+    field: 'co2',
     headerName: 'Co2 (PPM)',
     type: 'number',
   },
   {
-    field: 'RuidoSonoro',
+    field: 'ruidoSonoro',
     headerName: 'RuidoSonoro (db)',
     type: 'number',
   },
   {
-    field: 'Luminosidade',
+    field: 'luminosidade',
     headerName: 'Luminosidade',
     type: 'number',
   },
   {
-    field: 'Temperatura',
+    field: 'temperatura',
     headerName: 'Temperatura (°C)',
     type: 'number',
   },
   {
-    field: 'COVT',
+    field: 'tvoc',
     headerName: 'COVT (PPB)',
     type: 'number',
   },
   {
-    field: 'Umidade',
+    field: 'umidade',
     headerName: 'Umidade (%)',
     type: 'number',
   },
 ];
 
-const rows = [
+/*const rows = [
   {  nome: 'Nome 1', id: 1, idLeito: 1, Co2: 1, RuidoSonoro: 1, Luminosidade: 1, Temperatura: 1, COVT: 1, Umidade: 1 },
   {  nome: 'Nome 2', id: 2, idLeito: 2, Co2: 2, RuidoSonoro: 2, Luminosidade: 2, Temperatura: 2, COVT: 2, Umidade: 2 },
   {  nome: 'Nome 3', id: 3, idLeito: 3, Co2: 3, RuidoSonoro: 3, Luminosidade: 3, Temperatura: 3, COVT: 3, Umidade: 3 },
-];
+];*/
 
 const ReportNew = () => {
   let [relatorios, setRelatorios] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  //let {  nome, id, idLeito, Co2, RuidoSonoro, Luminosidade, Temperatura, COVT, Umidade } = relatorios;
+
+
 
   useEffect(()=>{
     
@@ -79,15 +83,27 @@ const ReportNew = () => {
        
   
   },[])
-
-  console.log(relatorios);
+  const linhas = relatorios.map( ({ idRelatorio, nome, id, idLeito, co2, ruido, luminosidade, temperatura, tvoc, umidade }) => ({
+    nome: nome, 
+    id: idRelatorio,
+    idLeito: idLeito,
+    co2: co2,
+    ruidoSonoro: ruido,
+    luminosidade: luminosidade,
+    temperatura: temperatura,
+    tvoc: tvoc,
+    umidade: umidade,
+    key: idRelatorio
+  }));
+  console.log("relatorio:" + relatorios);
+  console.log("linhas: " + linhas);
 
   return (
     <Container maxWidth="lg" >
         <Box>
     <div style={{ height: 400, width: '100%' }}>
       <DataGrid
-        rows={rows}
+        rows={linhas}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
